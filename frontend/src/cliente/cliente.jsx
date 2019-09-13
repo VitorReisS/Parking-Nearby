78cd49affe00db0694b49d578a45fb87ef76e39c
VitorReisS <vitor20101983@gmail.com>
2019-09-13 10:01:16 -0300

    Componentes Cliente e About

---
+++ frontend/src/cliente/cliente.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Cliente extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Cliente</h1>
+            </div>
+        )
+    }
+}
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,12 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Cliente from '../cliente/cliente'
+import About from '../about/about'
 
 export default props => (
     <div className='container'>
-        <h1>Teste</h1>
+        <Cliente />
+        <About />
     </div>
 )
