d308266a402b043e5c943a5a980c3f7d1e8a7dd4
VitorReisS <vitor20101983@gmail.com>
2019-09-13 10:16:47 -0300

    Componente Menu

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,9 +4,11 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
 import Cliente from '../cliente/cliente'
 import About from '../about/about'
+import Menu from '../template/menu'
 
 export default props => (
     <div className='container'>
+        <Menu />
         <Cliente />
         <About />
     </div>
