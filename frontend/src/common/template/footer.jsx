6ca0693dbb28c839691496999e447fbca12a8b95
VitorReisS <vitor20101983@gmail.com>
2019-10-04 14:54:12 -0300

    Componente Rodapé

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 export default props => (
     <footer className='main-footer'>
         <strong>
-            Copyright &copy; 2019
-<a href='http://cod3r.com.br' target='_blank'> Cod3r</a>.
-</strong>
+            Parking Nearby &copy; 2019
+        </strong>
     </footer>
 )
