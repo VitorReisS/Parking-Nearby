1abab9e87d8ab8a3b5d99e984f43138a1ad31b82
VitorReisS <vitor20101983@gmail.com>
2019-10-04 15:08:16 -0300

    Componente Cabeçalho

--- frontend/src/common/template/header.jsx
+++ frontend/src/common/template/header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default props => (
     <header className='main-header'>
         <a href='/#/' className='logo'>
-            <span className='logo-mini'><b>My</b>M</span>
+            <span className='logo-mini'><b>Parking</b>Nearby</span>
             <span className='logo-lg'>
-                <i className='fa fa-money'></i>
-                <b> My</b> Money
+                <i className='fa fa-car'></i>
+                <b> Parking</b> Nearby
             </span>
         </a>
         <nav className='navbar navbar-static-top'>
