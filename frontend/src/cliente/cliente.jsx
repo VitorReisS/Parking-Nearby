c04dbe67af245a3421f0c31cad71d8040b9eb70b
VitorReisS <vitor20101983@gmail.com>
2019-10-18 17:44:53 -0300

    Componente Tab Content

--- frontend/src/cliente/cliente.jsx
+++ frontend/src/cliente/cliente.jsx
@@ -6,6 +6,7 @@ import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
+import TabContent from '../common/tab/tabContent'
 
 class Cliente extends Component {
 
@@ -18,11 +19,14 @@ class Cliente extends Component {
                         <TabsHeader>
                             <TabHeader label='Listar' icon='bars' target='tabList' />
                             <TabHeader label='Incluir' icon='plus' target='tabCreate' />
-                            <TabHeader label='Alterar' icon='pencil'  target='tabUpdate' />
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
                         </TabsHeader>
                         <TabsContent>
-
+                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
