f47949bcf517e6a59b90df7b3c56a1bf9f82c95d
VitorReisS <vitor20101983@gmail.com>
2019-10-18 17:21:50 -0300

    Componentes Estruturais das Abas

--- frontend/src/cliente/cliente.jsx
+++ frontend/src/cliente/cliente.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabHeader'
+import TabsContent from '../common/tab/tabContent'
 
 class Cliente extends Component {
 
@@ -10,7 +13,14 @@ class Cliente extends Component {
             <div>
                 <ContentHeader title='Clientes' small='Cadastro' />
                 <Content>
-                    Clientes
+                    <Tabs>
+                        <TabsHeader>
+
+                        </TabsHeader>
+                        <TabsContent>
+                            
+                        </TabsContent>
+                    </Tabs>
                 </Content>
             </div>
         )
