742f1449a935cfe4edc5328888061031c7d87d7d
VitorReisS <vitor20101983@gmail.com>
2019-10-18 17:15:42 -0300

    Componente Cliente: Refatorando para Classe

--- frontend/src/cliente/cliente.jsx
+++ frontend/src/cliente/cliente.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-    <div>
-        <h1>Cliente</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+class Cliente extends Component {
+
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Clientes' small='Cadastro' />
+                <Content>
+                    Clientes
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default Cliente
