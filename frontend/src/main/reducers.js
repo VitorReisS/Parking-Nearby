2b0731c0466c98f51e0ee612a5e9ca79e658e7b8
VitorReisS <vitor20101983@gmail.com>
2019-10-18 17:36:16 -0300

    Criando a Primeira Action e o Reducer

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
-const rootReducer = combineReducers({
+import TabReducer from '../common/tab/tabReducer'
 
+const rootReducer = combineReducers({
+    tab: TabReducer
 })
 
 export default rootReducer
