7b01a1ca1e4ba6bba63f66b9ffde17801ac04a3b
VitorReisS <vitor20101983@gmail.com>
2019-10-19 13:50:04 -0300

    Componente Estacionamento: Refatorando para Classe

--- frontend/src/estacionamento/estacionamento.jsx
+++ frontend/src/estacionamento/estacionamento.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 
-class Funcionario extends Component {
+class Estacionamento extends Component {
     render() {
         return (
             <div>
@@ -16,4 +16,4 @@ class Funcionario extends Component {
     }
 }
 
-export default Funcionario
+export default Estacionamento
