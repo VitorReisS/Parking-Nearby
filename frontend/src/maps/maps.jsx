ed0d7857f46bc57622f10e8836ae6c0e66fd5f9e
VitorReisS <vitor20101983@gmail.com>
2019-11-07 00:52:26 -0300

    Adicionando Maps

--- frontend/src/maps/maps.jsx
+++ frontend/src/maps/maps.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import Foto from '../img/foto.png'
 
 class Maps extends Component {
     render() {
@@ -9,7 +10,7 @@ class Maps extends Component {
             <div>
                 <ContentHeader title='Maps' small='Versão 1.0' />
                 <Content>
-                    
+                    <img src={Foto} width="1100" height="500" />
                 </Content>
             </div>
         )
